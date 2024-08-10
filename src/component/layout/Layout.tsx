4a7ca8950ca8a58fbbb1e31ae716c0446f77d8d1
yeonghun-yun <lurutia@naver.com>
2024-08-10 12:06:44 +0900

    layout 추가

---
+++ src/component/layout/Layout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router"
+
+function AdminLayout() {
+    return (
+        <>
+            <div>admin layout</div>
+            <Outlet />
+        </>
+    )
+  }
+  
+  export default AdminLayout
+  
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,15 @@
 import { Route, Routes } from "react-router-dom"
 import StoreRoute from './storeRoute'
+import LoginPage from '../pages/login'
+import AdminLayout from '../component/layout/Layout'
 
 function App() {
   return (
     <Routes>
-      <Route path="/stores/*" element={<StoreRoute />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/bo/*" element={<AdminLayout />}>
+        <Route path="stores/*" element={<StoreRoute />} />
+      </Route>
     </Routes>
   );
 }
