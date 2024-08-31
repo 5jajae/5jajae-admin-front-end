377fd7580ada9d3e000ee50f88c53ed70c474e5b
yeonghun-yun <lurutia@naver.com>
2024-08-31 19:36:51 +0900

    feat: 로그인 페이지 디자인 변경

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -4,6 +4,31 @@ import InlineForm from '../../component/form/InlineForm.tsx';
 import { LoginRequestForm } from "../../types/login/login.ts";
 import LoginService from "../../api/login/LoginService.ts";
 import { useNavigate } from "react-router";
+import styled from 'styled-components';
+
+const ContainerDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    position: relative;
+
+    form {
+        width: 24rem;
+        max-width: 90%;
+        z-index: 1;
+        animation: showSignInForm 1s;
+    }
+
+    form img {
+        width: 4rem;
+    }
+`;
 
 function LoginPage() {
     
@@ -30,23 +55,33 @@ function LoginPage() {
             password
         } as LoginRequestForm;
 
+        if (!requestForm.username || !requestForm.password) {
+            alert('아이디 또는 패스워드를 입력해주세요.');
+            return;
+        }
+
         LoginService.login(requestForm).then(() => {
             navigate('/');
+        }).catch((reason) => {
+            alert(reason?.response?.data?.message);
         })
     }
 
     return (
-        <Container fluid>
-            <div>login page</div>
-            <InlineForm width={800}>
-                <Form.Control type="text" value={id || ''} onChange={handleIdInput} />
-            </InlineForm>
-            <InlineForm width={800}>
-                <Form.Control type="password" value={password || ''} onChange={handlePasswordInput} />
-            </InlineForm>
-            <Button type="button" variant="primary" onClick={handleSubmit}>로그인</Button>
-        </Container>
-        
+        <ContainerDiv>
+            <Form className="shadow p-4 bg-white rounded">
+                <Image src="/logo-black.png" alt="logo" style={{ width: '160px' }} />
+                <Form.Group className="mb-2" controlId="formBasicEmail">
+                    <Form.Label>아이디</Form.Label>
+                    <Form.Control type="text" value={id || ''} onChange={handleIdInput} />
+                </Form.Group>
+                <Form.Group className="mb-2" controlId="formBasicEmail">
+                    <Form.Label>패스워드</Form.Label>
+                    <Form.Control type="password" value={password || ''} onChange={handlePasswordInput} />
+                </Form.Group>
+                <Button style={{marginTop: 10}} type="button" variant="primary" onClick={handleSubmit}>로그인</Button>
+            </Form>
+      </ContainerDiv>
     )
 }
   
